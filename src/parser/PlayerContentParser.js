ca0022c84906d838556e6f7d71c849741da76436
Svit69 <ol.svitenkov@gmail.com>
2025-12-05 15:01:52 +0500

    Tighten match stats detection to statTable/table rows

--- src/parser/PlayerContentParser.js
+++ src/parser/PlayerContentParser.js
@@ -6,13 +6,18 @@ class PlayerContentParser extends AbstractContentParser {
     '#wrapper > div.players > div > div > section:nth-child(1) > div > div > div.frameCard-header__detail > div.frameCard-header__detail-header > div:nth-child(1) > div > span:nth-child(1)';
   #positionSelector =
     '#wrapper > div.players > div > div > section:nth-child(1) > div > div > div.frameCard-header__detail > div.frameCard-header__detail-header > div:nth-child(3) > p.frameCard-header__detail-local.roboto.roboto-normal.roboto-xxl.color-black';
-  #matchStatsSelector = 'div.players-detail';
+  #matchStatsSelector = 'div.players-detail__statTable, #table_all_games';
 
   parseContent(html) {
     const $ = cheerio.load(html);
     const name = $(this.#nameSelector).text().trim();
     const position = $(this.#positionSelector).text().trim();
-    const hasMatchStats = $(this.#matchStatsSelector).first().length > 0;
+    const statsBlock = $(this.#matchStatsSelector).first();
+    const hasMatchStats =
+      statsBlock.length > 0 &&
+      (statsBlock.is('table')
+        ? statsBlock.find('tbody tr').length > 0
+        : statsBlock.find('table').length > 0);
 
     if (!name) {
       throw new Error('Не удалось найти имя игрока по заданному селектору');
